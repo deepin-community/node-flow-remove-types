35127a3dfc0219187ec08a967028db65f6b349b4
Deepin Developer <deepin-dev@deepin.org>
2022-10-13 15:21:35 +0800

    feat: Init commit
    init node-flow-remove-types
    
    Log:

---
+++ test-update.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Generate expected output
+./flow-remove-types test/source.js > test/expected.js;
+
+# Generate expected output with --pretty flag
+./flow-remove-types --pretty test/source.js > test/expected-pretty.js;
+
+# Test expected source maps with --pretty --sourcemaps
+./flow-remove-types --pretty --sourcemaps test/source.js -d test/expected-with-maps;
+
+# Test expected source maps with --pretty --sourcemaps inline
+./flow-remove-types --pretty --sourcemaps inline test/source.js > test/expected-pretty-inlinemap.js;
